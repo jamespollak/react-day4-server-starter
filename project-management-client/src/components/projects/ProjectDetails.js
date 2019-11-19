65f4557ffb344939889436717ccdcf5bac65f0be
Gijs <gijs@MacBook-Pro-di-Gebruiker.local>
2019-11-19 21:13:49 +0100

    push it real good

---
+++ project-management-client/src/components/projects/ProjectDetails.js
@@ -0,0 +1,41 @@
+// components/projects/ProjectDetails.js
+
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+class ProjectDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.getSingleProject();
+  }
+
+  getSingleProject = () => {
+    const { params } = this.props.match;
+    axios
+      .get(`http://localhost:5000/api/projects/${params.id}`)
+      .then(responseFromApi => {
+        const theProject = responseFromApi.data;
+        this.setState(theProject);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <p>{this.state.description}</p>
+        <Link to={"/projects"}>Back to projects</Link>
+      </div>
+    );
+  }
+}
+
+export default ProjectDetails;
